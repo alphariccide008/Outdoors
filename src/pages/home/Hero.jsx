05f33d8629afa5f5cb22fe08a1011adbe8ceaa40
alpha <godspowerlawrence008@gmail.com>
2024-12-21 18:17:27 -0500

    new update

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,5 +1,6 @@
 import './Home.css'
 import { Link } from "react-router-dom";
+import { FaArrowRight } from 'react-icons/fa';
 
 const Hero = () => {
   return (
@@ -8,12 +9,12 @@ const Hero = () => {
             <div className="flex-col md:w-1/2">
                 <h1 className='text-3xl pb-3 text-center md:text-start'>Increase <span className='text-[#FD6821]'>Awareness</span></h1>
                 <h1 className='text-3xl pb-5 text-center md:text-start'>Drive Sales</h1>
-                <p className='pr-[5%] text-center md:text-start'>increase brand visibility and revenue with Outdoor advertising in Nigeria</p>
+                <p className='pr-[5%] text-center md:text-start text-sm'>increase brand visibility and revenue with Outdoor advertising in Nigeria</p>
                 
-                <div className="flex pb-32 mt-2">
+                <div className="flex pb-32 mt-4">
                     <Link to="#"
-                    className="py-2 px-6 text-white bg-[#0A6535] rounded-3xl hover:bg-[#085827] transition-all duration-300" style={{fontSize: '12px'}}>
-                        Get Started
+                    className="py-2 flex px-5 text-white bg-[#0A6535] rounded-3xl hover:bg-[#085827] transition-all duration-300" style={{fontSize: '12px'}}>
+                        Get Started <FaArrowRight className='mt-1 mx-1'/>
                     </Link>
                     <Link to="#"
                     className="py-2  mx-4 px-2 text-sm text-[#085827] border border-[#085827] rounded-3xl hover:bg-[#085827] transition-all duration-300" style={{fontSize: '12px'}}>

de79780cfdc7137d0bc88415cc2fa62f1d661eaf
alpha <godspowerlawrence008@gmail.com>
2024-12-21 18:02:32 -0500

    new update

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -10,19 +10,15 @@ const Hero = () => {
                 <h1 className='text-3xl pb-5 text-center md:text-start'>Drive Sales</h1>
                 <p className='pr-[5%] text-center md:text-start'>increase brand visibility and revenue with Outdoor advertising in Nigeria</p>
                 
-                <div className="flex pb-32">
-                    <div className="w-1/2">
+                <div className="flex pb-32 mt-2">
                     <Link to="#"
-                    className="py-2 px-6 text-white bg-[#0A6535] rounded-3xl hover:bg-[#085827] transition-all duration-300">
+                    className="py-2 px-6 text-white bg-[#0A6535] rounded-3xl hover:bg-[#085827] transition-all duration-300" style={{fontSize: '12px'}}>
                         Get Started
                     </Link>
-                    </div>
-                   <div className="w-1/2 text-sm">
-                   <Link to="#"
-                    className="py-2  mx-4 px-3 text-[#085827] border border-[#085827] rounded-3xl hover:bg-[#085827] transition-all duration-300">
+                    <Link to="#"
+                    className="py-2  mx-4 px-2 text-sm text-[#085827] border border-[#085827] rounded-3xl hover:bg-[#085827] transition-all duration-300" style={{fontSize: '12px'}}>
                         Get discounted Billboard
                     </Link>
-                   </div>
                 </div>
 
             </div>
